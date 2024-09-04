2c2938b7ecf664e4d72776a70c80fd8271236e72
João Vitor Nunes <jvnunes145@gmail.com>
2024-09-04 19:45:57 -0300

    Ajustes para otimizar.

--- main.R
+++ main.R
@@ -4,16 +4,11 @@ library(dplyr)
 library(yaml)
 library(jsonlite)
 library(ggplot2)
-<<<<<<< HEAD
 library(glmnet)
 source("scripts/treinamento.R")
 source("scripts/predicao.R")
 source("scripts/graficos.R")
-=======
-source("treinamento.R")
-source("predicao.R")
-source("graficos.R")
->>>>>>> 4f6a844d7b49f2f635109e061a321f174c70657d
+
 
 config = read_yaml("configuracao.yaml")
 dados = read_csv(glue("entradas/{config$dados}"), show_col_types = FALSE)
@@ -29,21 +24,10 @@ vec = numeric(length(config$variaveis$Resposta))
     vec[i] = config$variaveis$Resposta[[i]]
 }
 
-<<<<<<< HEAD
 write_rds(fit, "saidas/fit.rds")
 write_json(toJSON(predict_function(pred)), "saidas/predicoes_regressao.json")
 ggsave("saidas/grafico.pdf", plot = graficos(predict(fit, newx = as.matrix(dados[vec])), 
                                              dados[config$variaveis$Preditoras$y], 
                                              predict_function(pred)))
 rm(list = ls())
-=======
-fitted = predict(fit, newx = as.matrix(dados[vec]))
 
-predicoes = predict_function(pred)
-x = dados[config$variaveis$Preditoras$y] - fitted
-y = dados[config$variaveis$Preditoras$y]
-
-write_rds(fit, "saidas/fit.rds")
-write_json(toJSON(predict_function(pred)), "saidas/predicoes_regressao.json")
-ggsave("saidas/grafico.pdf", plot = graficos(x, y, predicoes))
->>>>>>> 4f6a844d7b49f2f635109e061a321f174c70657d
--- scripts/graficos.R
+++ scripts/graficos.R
@@ -10,8 +10,8 @@ graficos = function(x, y, predicoes) {
   g = df %>% 
     ggplot(aes(x = x, y = y)) +
     geom_point() +
-    geom_vline(xintercept = predicoes, color = "red", linetype = "dashed", show.legend = TRUE, size = 0.5) +
-    geom_vline(data = legend_data, aes(xintercept = intercept, color = color), linetype = "dashed", size = 0.5, show.legend = TRUE) + 
+    geom_vline(xintercept = predicoes, color = "red", linetype = "dashed", show.legend = TRUE, linewidth = 0.5) +
+    geom_vline(data = legend_data, aes(xintercept = intercept, color = color), linetype = "dashed", linewidth = 0.5, show.legend = TRUE) + 
     scale_color_manual(name = "Legenda", values = c("Valores preditos" = "red")) +
     labs(x = "Valores preditos no conjunto de dados", 
          y = "Valores observados", 
